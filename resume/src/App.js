437b22f43241881c714c7566e81feed89ca2f1fc
outofblue-eng <littlestran@gmail.com>
2022-11-10 20:48:36 -0500

    added about, contact,data--- about to move data from objects to data.js

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,25 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          React.createElement('div',null,"hello world");
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className = "text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
-
-export default App;
--- resume/src/components/attributes/attributes.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Attributes extends Component {
-  render() {
-    return <div>Attributes</div>;
-  }
-}
-
-export default Attributes;
